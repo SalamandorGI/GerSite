bd90e95c961b9eea6c217a02bd70f464690f94d4
SalamandorGI <131532586+SalamandorGI@users.noreply.github.com>
2023-04-22 19:39:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('section').hide().slideDown(1500); // скрываем секции и плавно их показываем
+});
+
+$(document).ready(function() {
+    $('header').hide().fadeIn(700); // скрываем секции и плавно их показываем
+});
