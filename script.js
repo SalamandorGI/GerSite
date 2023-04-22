0cd1b43924b03cd451327737e67cbed245e4b797
SalamandorGI <131532586+SalamandorGI@users.noreply.github.com>
2023-04-22 18:35:59 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('section').hide().slideDown(1500); // скрываем секции и плавно их показываем
-});
-
-$(document).ready(function() {
-    $('header').hide().fadeIn(700); // скрываем секции и плавно их показываем
-});
